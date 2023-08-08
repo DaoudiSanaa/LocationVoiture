048b0f32d2214382f47315ccd3039e53d5010bd7
DaoudiSanaa <daoudisanaa879@gmail.com>
2023-08-08 19:16:33 +0200

    initialisation de projet

---
+++ src/test/java/org/mql/location/LocationVoitureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mql.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationVoitureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
